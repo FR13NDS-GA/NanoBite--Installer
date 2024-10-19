ad9570e0933ce3009f49fad8d7db5171e5311430
FR13NDS-GA <devan200645@gmail.com>
2024-10-19 01:45:22 -0400

    Update Run_Install.sh

--- Run_Install.sh
+++ Run_Install.sh
@@ -8,4 +8,7 @@ cmdline=$1
 if [ $cmdline == "NanoBite-get--install__Ghost" ]; then
   chmod +x ~/Documents/NanoBite--Installer/pkgs/Ghost/NanoBite-get--install.sh
   ./~/Documents/NanoBite--Installer/pkgs/Ghost/NanoBite-get--install.sh
+else
+  echo "error! pkg does not exist"
 fi
+

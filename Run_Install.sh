640f4360e648836e8d91857e9dabc53cb1c70b53
FR13NDS-GA <devan200645@gmail.com>
2024-10-19 01:38:18 -0400

    Update Run_Install.sh

--- Run_Install.sh
+++ Run_Install.sh
@@ -1,1 +1,11 @@
+if [ -z "$1" ]; then
+  echo "Usage: $0 <cmdline>"
+  exit 1
+fi
 
+cmdline=$1
+
+if [ $cmdline == "NanoBite-get--install__Ghost" ]; then
+  chmod +x ~/Documents/NanoBite--Installer/pkgs/Ghost/NanoBite-get--install.sh
+  ./~/Documents/NanoBite--Installer/pkgs/Ghost/NanoBite-get--install.sh
+fi
